5f334284e49e948c1a9c6b351f55ec807b6f7b01
Austin Puckett <puckett_austin@yahoo.com>
2018-09-19 00:26:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 var recipes = {prop: 1};
 
-// function updateObjectWithKeyAndValue(object, key, value) {
-//   return Object.assign({}, object, {[key]: value});
-// }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
 
-// function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-//   object[key] = value;
-//   return object;
-// }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
-  delete newObj[key];
+  delete newObj.key;
   return newObj;
 }
 
-deleteFromObjectByKey(recipes, 'prop')
-
-// function deleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   
-// }
+}
 
-// function destructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   
-// }
+}
 
 
-// function destructivelyDeleteFromObjectByKey(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   
-// }
+}
 
 
 
